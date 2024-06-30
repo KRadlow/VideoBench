492bb1dbf66a64545ccaaae3d65d6b18e3b85806
KRadlow <k.radlowski458@gmail.com>
2024-06-30 22:03:56 +0200

    Add serilog

--- src/VideoBench.Web/Program.cs
+++ src/VideoBench.Web/Program.cs
@@ -1,10 +1,20 @@
 using Microsoft.Extensions.Options;
+using Serilog;
 using VideoBench.Application.Interfaces;
 using VideoBench.Application.Services;
 using VideoBench.Infrastructure.Clients;
 using VideoBench.Infrastructure.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add serilog services to the container
+var logger = new LoggerConfiguration().ReadFrom
+    .Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 // Configuration
 builder.Services.AddOptions<VideoApiConfig>()
